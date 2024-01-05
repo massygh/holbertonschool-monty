49a92fc04e1ccfc16e61524747d964e3e97c43ed
Ines-Oubabase <inesoubabas56@gmail.com>
2024-01-05 17:53:43 +0100

    code modifie

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "monty.h"
-
 /**
  * main - Monty interpreter main function
  * @argc: Argument count
@@ -15,149 +14,41 @@ int main(int argc, char *argv[])
 	stack_t *stack = NULL;
 
 	if (argc != 2)
-	{
 		error_usage();
-	}
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
+	if ((file = fopen(argv[1], "r")) == NULL)
 		error_file_open(argv[1]);
-	}
 	instruction_t opcodes[] = {
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL},
+		{"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop},
+		{NULL, NULL}
 	};
-	while (getline(&line, &len, file) != -1)
-	{
-		flag = 0;
+	while (getline(&line, &len, file) != -1) {
 		line_number++;
-		opcode = strtok(line, " \t$\n");
-		if (opcode == NULL || opcode[0] == '#')
+		if ((opcode = strtok(line, " \t$\n")) == NULL || opcode[0] == '#') 
 			continue;
-		if (strcmp(opcode, "push") == 0)
-		{
+	if ((flag = !strcmp(opcode, "push")))
+	{
 			value = strtok(NULL, " $\n");
-			if (value == NULL)
-			{
-				print_push_error(line_number, file, line);
-			}
-			for (i = 0; value[i] != '\0'; i++)
-			{
-				if  (!isdigit(value[i]) && value[i] != '-')
-				{
+			if (!value) print_push_error(line_number, file, line);
+			for (i = 0; value[i]; i++)
+				if (!isdigit(value[i]) && value[i] != '-')
 					print_push_error(line_number, file, line);
-				}
-			}
-		push(&stack, atoi(value));
-		flag = 1;
+			push(&stack, atoi(value));
 		}
-		if (flag == 0)
-		{
-			for (j = 0; opcodes[j].opcode != NULL; j++)
-			{
-				if (strcmp(opcode, opcodes[j].opcode) == 0)
+	if (!flag)
+	{
+			for (j = 0; opcodes[j].opcode; j++)
+				if (!strcmp(opcode, opcodes[j].opcode))
 				{
 					opcodes[j].f(&stack, line_number);
 					break;
 				}
-			}
+			if (j == 6) invalid_inst(line_number, opcode, file, line, stack);
 		}
-		if (j == 6)
-			invalid_inst(line_number, opcode, file, line, stack);
-	free(line);
-	line = NULL;
+		free(line);
+		line = NULL;
 	}
 	free_dlistint(stack);
-	free(line);
 	fclose(file);
 	return (0);
-}
-
-/**
- * push - Pushes an element onto the stack
- * @stack: Pointer to the stack
- * @value: Value to be pushed onto the stack
- */
-
-void push(stack_t **stack, int value)
-{
-	stack_t *new_node;
-
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
-
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
-}
-
-/**
- * free_dlistint - function that frees a list
- * @stack: pointer to free tmp
- * Return: void
- */
-
-void free_dlistint(stack_t *stack)
-{
-	stack_t *tmp;
-
-	while (stack != NULL)
-	{
-		tmp = stack;
-		stack = stack->next;
-		free(tmp);
-	}
-}
-
-/**
- * dlistint_len - function that returns the number of elements in a linked list
- * @stack: pointer to the counter nodes
- * Return: the numbers of nodes
- */
-
-size_t dlistint_len(const stack_t *stack)
-{
-	size_t i = 0;
-	const stack_t *actual = stack;
-
-	while (actual != NULL)
-	{
-		actual = actual->next;
-		i++;
-	}
-	return (i);
-}
-
-/**
- * sum_dlistint - function that adds the top two elements of the stack
- * @stack : pointer to list
- * Return: sum of top two elements or the stack
- */
-
-int sum_dlistint(stack_t *stack)
-
-{
-	int sum = 0, i = 0;
-
-	stack_t *current = stack;
-
-	while (i < 2)
-	{
-		sum += current->n;
-		current = current->next;
-		i++;
-	}
-	return (sum);
 }
