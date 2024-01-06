723074a613d9144936461feec2a4b96b4d84ef07
Ines-Oubabase <inesoubabas56@gmail.com>
2024-01-06 13:08:12 +0100

    nombre de ligne modifié

--- main.c
+++ main.c
@@ -7,49 +7,62 @@
  */
 int main(int argc, char *argv[])
 {
+	char *opcode, *value, *line = NULL;
+	size_t line_number = 0, len = 0;
 	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	ssize_t read;
-	unsigned int line_number = 0;
-	char *opcode;
-	instruction_t instructions[] = {
-		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
-		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
-	};
-	int i;
+	int i, j, flag;
 	stack_t *stack = NULL;
 
 	if (argc != 2)
-	{
 		error_usage();
-		return (EXIT_FAILURE);
-	}
+
 	file = fopen(argv[1], "r");
-	if (!file)
-	{
+	if (file == NULL)
 		error_file_open(argv[1]);
-		return (EXIT_FAILURE);
-	}
-	while ((read = getline(&line, &len, file)) != -1)
+
+	instruction_t opcodes[] = {
+		{"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL},
+	};
+	while (getline(&line, &len, file) != -1)
 	{
+		flag = 0;
 		line_number++;
-		opcode = strtok(line, " \t\n");
+		opcode = strtok(line, " \t$\n");
 		if (opcode == NULL || opcode[0] == '#')
 			continue;
-		for (i = 0; instructions[i].opcode; i++)
+		if (strcmp(opcode, "push") == 0)
 		{
-			if (strcmp(opcode, instructions[i].opcode) == 0)
+			value = strtok(NULL, " $\n");
+			if (value == NULL)
+				print_push_error(line_number, file, line);
+
+			for (i = 0; value[i] != '\0'; i++)
 			{
-				instructions[i].f(&stack, line_number);
-				break;
+				if  (!isdigit(value[i]) && value[i] != '-')
+					print_push_error(line_number, file, line);
+			}
+			push(&stack, atoi(value));
+			flag = 1;
+		}
+		if (flag == 0)
+		{
+			for (j = 0; opcodes[j].opcode != NULL; j++)
+			{
+				if (strcmp(opcode, opcodes[j].opcode) == 0)
+				{
+					opcodes[j].f(&stack, line_number);
+					break;
+				}
 			}
 		}
-		if (instructions[i].opcode == NULL)
+		if (j == 6)
 			invalid_inst(line_number, opcode, file, line, stack);
+		free(line);
+		line = NULL;
 	}
+	free_dlistint(stack);
 	free(line);
 	fclose(file);
-	free_dlistint(stack);
-	return (EXIT_SUCCESS);
+	return (0);
 }
