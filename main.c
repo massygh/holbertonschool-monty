f52a4be26f798daafb4f4d5e8fb35f5d8ba800ee
Ines-Oubabase <inesoubabas56@gmail.com>
2024-01-06 12:50:19 +0100

    modified the code

--- main.c
+++ main.c
@@ -1,78 +1,55 @@
 #include "monty.h"
 /**
- * main - Monty interpreter main function
- * @argc: Argument count
- * @argv: Argument values
- * Return: 0 on success, 1 on failure
+ * main - Entry point of the stack-based interpreter program.
+ * @argc: Argument count.
+ * @argv: Argument vector, argv[1] is expected to be the file to interpret.
+ * Return: EXIT_SUCCESS on success, or EXIT_FAILURE on error.
  */
 int main(int argc, char *argv[])
 {
-	char *opcode, *value, *line = NULL;
-	size_t line_number = 0, len = 0;
 	FILE *file;
-	int i, j, flag;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	unsigned int line_number = 0;
+	char *opcode;
+	instruction_t instructions[] = {
+		{"push", push}, {"pall", pall}, {"pint", pint}, {"pop", pop},
+		{"swap", swap}, {"add", add}, {"nop", nop}, {NULL, NULL}
+	};
+	int i;
 	stack_t *stack = NULL;
 
 	if (argc != 2)
 	{
 		error_usage();
+		return (EXIT_FAILURE);
 	}
 	file = fopen(argv[1], "r");
-	if (file == NULL)
+	if (!file)
 	{
 		error_file_open(argv[1]);
+		return (EXIT_FAILURE);
 	}
-	instruction_t opcodes[] = {
-		{"pall", pall},
-		{"pint", pint},
-		{"pop", pop},
-		{"swap", swap},
-		{"add", add},
-		{"nop", nop},
-		{NULL, NULL},
-	};
-	while (getline(&line, &len, file) != -1)
+	while ((read = getline(&line, &len, file)) != -1)
 	{
-		flag = 0;
 		line_number++;
-		opcode = strtok(line, " \t$\n");
+		opcode = strtok(line, " \t\n");
 		if (opcode == NULL || opcode[0] == '#')
 			continue;
-		if (strcmp(opcode, "push") == 0)
+		for (i = 0; instructions[i].opcode; i++)
 		{
-			value = strtok(NULL, " $\n");
-			if (value == NULL)
+			if (strcmp(opcode, instructions[i].opcode) == 0)
 			{
-				print_push_error(line_number, file, line);
-			}
-			for (i = 0; value[i] != '\0'; i++)
-			{
-				if  (!isdigit(value[i]) && value[i] != '-')
-				{
-					print_push_error(line_number, file, line);
-				}
-			}
-		push(&stack, atoi(value));
-		flag = 1;
-		}
-		if (flag == 0)
-		{
-			for (j = 0; opcodes[j].opcode != NULL; j++)
-			{
-				if (strcmp(opcode, opcodes[j].opcode) == 0)
-				{
-					opcodes[j].f(&stack, line_number);
-					break;
-				}
+				instructions[i].f(&stack, line_number);
+				break;
 			}
 		}
-		if (j == 6)
+		if (instructions[i].opcode == NULL)
 			invalid_inst(line_number, opcode, file, line, stack);
-	free(line);
-	line = NULL;
 	}
-	free_dlistint(stack);
 	free(line);
 	fclose(file);
-	return (0);
+	free_dlistint(stack);
+	return (EXIT_SUCCESS);
 }
