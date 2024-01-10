34b329f2961bc94f1935268ac755ddd6a1185107
massygh <146716580+massygh@users.noreply.github.com>
2024-01-10 23:30:06 +0100

    Update push.c

--- push.c
+++ push.c
@@ -1,24 +1,29 @@
 #include "monty.h"
+
 /**
- * push - Pushes an element onto the stack
- * @stack: Pointer to the stack
- * @value: Value to be pushed onto the stack
+ * push - Ajoute un élément au sommet de la pile.
+ * @stack: Pointeur vers la pile
+ * @value: Valeur à ajouter à la pile
  */
 void push(stack_t **stack, int value)
 {
-	stack_t *new_node;
+    stack_t *new_node;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-	new_node->n = value;
-	new_node->prev = NULL;
-	new_node->next = *stack;
+    // Alloue de la mémoire pour le nouveau nœud
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL)
+    {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
 
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+    // Initialise le nouveau nœud avec la valeur donnée
+    new_node->n = value;
+    new_node->prev = NULL;
+    new_node->next = *stack;
+
+    // Met à jour les liens de la pile
+    if (*stack != NULL)
+        (*stack)->prev = new_node;
+    *stack = new_node;
 }
