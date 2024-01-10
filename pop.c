bbce7f707227298d0d7b144661f202fc3a05c67d
massygh <146716580+massygh@users.noreply.github.com>
2024-01-10 23:30:43 +0100

    Update pop.c

--- pop.c
+++ pop.c
@@ -1,26 +1,26 @@
 #include "monty.h"
 
 /**
- * pop - func that delete a new node
- * @stack: pointer to the stack
- * @line_number: Line number in the script
+ * pop - Supprime le nœud supérieur de la pile.
+ * @stack: Pointeur vers la pile
+ * @line_number: Numéro de ligne dans le script
  */
-
 void pop(stack_t **stack, unsigned int line_number)
 {
-	stack_t *node_to_delete;
-	(void)line_number;/*Parametro no Utilizado*/
+    stack_t *node_to_delete;
+    (void)line_number; // Paramètre non utilisé
 
-	if (*stack == NULL)
-	{
-		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-		exit(EXIT_FAILURE);
-	}
+    // Vérifie si la pile est vide
+    if (*stack == NULL)
+    {
+        fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	node_to_delete = *stack;
-
-	*stack = node_to_delete->next;
-	if (node_to_delete->next)
-		node_to_delete->next->prev = NULL;
-	free(node_to_delete);
+    // Supprime le nœud supérieur et met à jour les liens
+    node_to_delete = *stack;
+    *stack = node_to_delete->next;
+    if (node_to_delete->next)
+        node_to_delete->next->prev = NULL;
+    free(node_to_delete);
 }
