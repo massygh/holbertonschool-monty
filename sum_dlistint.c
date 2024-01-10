4c260b91106cb9af12ac53023e895416f79b8c96
massygh <146716580+massygh@users.noreply.github.com>
2024-01-10 23:29:23 +0100

    Update sum_dlistint.c

--- sum_dlistint.c
+++ sum_dlistint.c
@@ -1,21 +1,22 @@
 #include "monty.h"
+
 /**
- * sum_dlistint - function that adds the top two elements of the stack
- * @stack : pointer to list
- * Return: sum of top two elements or the stack
+ * sum_dlistint - Calcule la somme des deux éléments supérieurs de la pile.
+ * @stack: Pointeur vers la pile
+ * Return: Somme des deux éléments supérieurs de la pile
  */
 int sum_dlistint(stack_t *stack)
-
 {
-	int sum = 0, i = 0;
+    int sum = 0;
+    int i = 0;
+    stack_t *current = stack;
 
-	stack_t *current = stack;
-
-	while (i < 2)
-	{
-		sum += current->n;
-		current = current->next;
-		i++;
-	}
-	return (sum);
+    // Ajoute les valeurs des deux éléments supérieurs de la pile
+    while (i < 2)
+    {
+        sum += current->n;
+        current = current->next;
+        i++;
+    }
+    return sum;
 }
