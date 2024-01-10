d18d6f811b54f420210a02ccc47c620101d2df08
massygh <146716580+massygh@users.noreply.github.com>
2024-01-10 23:26:07 +0100

    Update swap.c

--- swap.c
+++ swap.c
@@ -1,28 +1,28 @@
 #include "monty.h"
 
 /**
- * swap - function to exchange values
- * @stack: Pointer to the stack
- * @line_number: Line number in the script
+ * swap - Échange les valeurs de deux nœuds adjacents dans la pile.
+ * @stack: Pointeur vers la pile
+ * @line_number: Numéro de ligne dans le script
  */
-
 void swap(stack_t **stack, unsigned int line_number)
 {
-	int temp2;
-	size_t nodes;
-	stack_t *node_to_swap = *stack;
-	(void)line_number;/*Parametro no Utilizado*/
+    int temp2;
+    size_t nodes;
+    stack_t *node_to_swap = *stack;
+    (void)line_number; // Paramètre non utilisé
 
-	nodes = dlistint_len(*stack);
+    // Vérifie si la pile a au moins deux éléments
+    nodes = dlistint_len(*stack);
+    if (nodes < 2)
+    {
+        fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+        exit(EXIT_FAILURE);
+    }
 
-	if (nodes < 2)
-	{
-		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	node_to_swap = *stack;
-	temp2 = node_to_swap->n;
-	node_to_swap->n = node_to_swap->next->n;
-	node_to_swap->next->n = temp2;
+    // Échange les valeurs des deux premiers nœuds
+    node_to_swap = *stack;
+    temp2 = node_to_swap->n;
+    node_to_swap->n = node_to_swap->next->n;
+    node_to_swap->next->n = temp2;
 }
