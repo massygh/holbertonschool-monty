69092e1afc8bd319c50dabd915b5fff82d7df10c
massygh <146716580+massygh@users.noreply.github.com>
2024-01-11 10:30:00 +0100

    Update dlistint_len.c

--- dlistint_len.c
+++ dlistint_len.c
@@ -1,4 +1,5 @@
 #include "monty.h"
+
 /**
  * dlistint_len - function that returns the number of elements in a linked list
  * @stack: pointer to the counter nodes
@@ -6,13 +7,14 @@
  */
 size_t dlistint_len(const stack_t *stack)
 {
-	size_t i = 0;
-	const stack_t *actual = stack;
+    size_t i = 0;  // Variable pour stocker le nombre de nœuds
+    const stack_t *actual = stack;  // Pointeur temporaire pour parcourir la liste
 
-	while (actual != NULL)
-	{
-		actual = actual->next;
-		i++;
-	}
-	return (i);
+    while (actual != NULL)
+    {
+        actual = actual->next;  // Déplacement vers le nœud suivant
+        i++;  // Incrémentation du compteur de nœuds
+    }
+
+    return (i);  // Retourne le nombre total de nœuds dans la liste
 }
